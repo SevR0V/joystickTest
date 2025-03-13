fbcf6142f7bfe3b1c107c9ebd8eb3c843dc38628
SevR0V <ermakovigorrov@gmail.com>
2025-03-13 21:42:00 +0300

    Убрано ненужное отображение изменения элемента

--- customlineedit.cpp
+++ customlineedit.cpp
@@ -2,12 +2,11 @@
 #include <QDebug>
 
 CustomLineEdit::CustomLineEdit(QWidget *parent) : QLineEdit(parent) {
-    setStyleSheet("border: 1px solid black; padding: 3px;");  // Визуально выделяем
 }
 
 void CustomLineEdit::mousePressEvent(QMouseEvent *event) {
     if (event->button() == Qt::LeftButton) {
-        emit leftClicked(objectName());  // Передаём имя объекта
+        emit leftClicked(objectName());
     } else if (event->button() == Qt::RightButton) {
         emit rightClicked(objectName());
     }
